7fa9f7aac9755e17045bab34a277906c902b5f6b
Rasha Shalan <rasha.shalan@brmaja.com>
2024-10-28 07:49:54 +0300

    controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\slider;
+use App\Models\social_links;
 
 class HomeController extends Controller
 {
@@ -11,7 +12,8 @@ class HomeController extends Controller
   public function index()
   {
     $sliders = Slider::all();
-    return view('homepage', compact('sliders'));
+    $social=social_links::first();
+    return view('homepage', compact('sliders','social'));
   }
 
   public function about()
--- app/Http/Controllers/admin/SocialController.php
+++ app/Http/Controllers/admin/SocialController.php
@@ -4,62 +4,96 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\social_links;
+use Illuminate\Support\Facades\Auth;
 
 class SocialController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    //
+    $social = social_links::first();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+    return view('admin/content/social/index', compact('social'));
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+    //dd($request->all());
+    $social = social_links::get();
+    if (count($social) == 0) {
+      $slider = new social_links();
+      $slider->facebook = $request->facebook ?? '';
+      $slider->twitter = $request->twitter ?? '';
+      $slider->gmail = $request->gmail ?? '';
+      $slider->whatsapp = $request->whatsapp ?? '';
+      $slider->linkedin = $request->linkedin ?? '';
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+      // $slider->created_by = Auth::user()->id;
+      $slider->created_at = now();
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+      $slider->save();
+    } else {
+      $slider = social_links::find(1);
+      $slider->facebook = $request->facebook ?? '';
+      $slider->twitter = $request->twitter ?? '';
+      $slider->gmail = $request->gmail ?? '';
+      $slider->whatsapp = $request->whatsapp ?? '';
+      $slider->linkedin = $request->linkedin ?? '';
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+      // $slider->updated_by = Auth::user()->id;
+      $slider->updated_at = now();
+      $slider->save();
     }
+    return redirect()
+      ->to(app()->getLocale() . '/dashboard/social')
+      ->with('message', 'Social added successfully');
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(string $id)
+  {
+    //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(string $id)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, string $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(string $id)
+  {
+    //
+  }
 }
